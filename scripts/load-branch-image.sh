018744b881fcdb60668a0d0de14b1a83bb364457
Johan Preynat <johan.preynat@gmail.com>
2018-05-31 23:58:17 +0200

    Revert "Move logic to scripts and fix loading current commit image"
    This reverts commit f7ecea5a2bd3b6249d740b30f49eac7a0c29a9f7.

--- scripts/load-branch-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o nounset
-set -o errexit
-set -o pipefail
-
-# Load the current branch image from cache if available
-if [[ -e $DOCKER_DIR/$IMAGE:$TRAVIS_BRANCH.lzo ]];
-    then lzop -d $DOCKER_DIR/$IMAGE:$TRAVIS_BRANCH.lzo | docker load;
-fi
